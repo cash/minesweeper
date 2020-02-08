25c2c6cbfb9abd71c65e49e8f7997b0ea94f620e
Cash Costello <cash.costello@gmail.com>
2020-02-08 08:33:44 -0500

    moved random AI into library

--- demo.py
+++ demo.py
@@ -1,36 +1,9 @@
-import random
-
 import minesweeper as ms
-
-
-class RandomAI(ms.GameAI):
-    def __init__(self):
-        self.width = 0
-        self.height = 0
-        self.exposed_squares = set()
-
-    def init(self, config):
-        self.width = config.width
-        self.height = config.height
-        self.exposed_squares.clear()
-
-    def next(self):
-        while True:
-            x = random.randint(0, self.width - 1)
-            y = random.randint(0, self.height - 1)
-            if (x, y) not in self.exposed_squares:
-                break
-        print('selecting point ({0},{1})'.format(x, y))
-        return x, y
-
-    def update(self, result):
-        for position in result.new_squares:
-            self.exposed_squares.add((position.x, position.y))
 
 
 num_games = 1
 config = ms.GameConfig()
-ai = RandomAI()
+ai = ms.RandomAI()
 viz = ms.GameVisualizer('key')
 results = ms.run_games(config, 1, ai, viz)
 if results[0].success:
--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -189,6 +189,31 @@ class GameAI(abc.ABC):
         return []
 
 
+class RandomAI(GameAI):
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        self.exposed_squares = set()
+
+    def init(self, config):
+        self.width = config.width
+        self.height = config.height
+        self.exposed_squares.clear()
+
+    def next(self):
+        while True:
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
+            if (x, y) not in self.exposed_squares:
+                break
+        print('selecting point ({0},{1})'.format(x, y))
+        return x, y
+
+    def update(self, result):
+        for position in result.new_squares:
+            self.exposed_squares.add((position.x, position.y))
+
+
 """
 Run a set of games to evaluate a GameAI
 
